7fc54ddbd0562c4ba6782076f81231742d6f23e6
Blake-Gallagher, Alison (UK - Belfast) <ablakegallagher@deloitte.co.uk>
2015-03-15 21:18:45 +0000

    two app pages

---
+++ app/src/main/java/com/example/ablakegallagher/padisonapp/HomePage.java
@@ -0,0 +1,17 @@
+package com.example.ablakegallagher.padisonapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View.OnClickListener;
+import android.view.View;
+
+/**
+ * Created by ablakegallagher on 14/03/2015.
+ */
+public class HomePage extends Activity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+    }
+}
--- app/src/main/java/com/example/ablakegallagher/padisonapp/MainActivity.java
+++ app/src/main/java/com/example/ablakegallagher/padisonapp/MainActivity.java
@@ -1,39 +1,33 @@
 package com.example.ablakegallagher.padisonapp;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button continueButton = (Button) findViewById(R.id.continue_button);
+        continueButton.setOnClickListener(new View.OnClickListener(){
+        //View.OnClickListener listener = new View.OnClickListener(){
+
+            public void onClick(View v) {
+                // open home page
+                Intent i = new Intent(MainActivity.this, HomePage.class);
+                MainActivity.this.startActivity(i);
+            }
+        });
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
+
